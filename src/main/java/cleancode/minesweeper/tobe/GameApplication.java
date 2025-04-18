752be3f34a8ea9ee75d200e78d212815d3ce35d9
kn01006 <ske.kn01006@partner.skenergy.com>
2025-04-18 14:37:05 +0900

    refactor: 책임 분리 by SRP

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,8 @@
+package cleancode.minesweeper.tobe;
+
+public class GameApplication {
+    public static void main(String[] args) {
+        MineSweeper mineSweeper = new MineSweeper();
+        mineSweeper.run();
+    }
+}
