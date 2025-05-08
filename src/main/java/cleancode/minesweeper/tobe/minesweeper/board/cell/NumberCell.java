4a21153db474888065ba1798cb91af5dfeda3d3d
kn01006 <ske.kn01006@partner.skenergy.com>
2025-05-08 14:31:30 +0900

    refactor: package 정리

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/NumberCell.java
@@ -0,0 +1,56 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+public class NumberCell implements Cell {
+
+    private final CellState cellState = CellState.initialize();
+
+    private int nearByLandMineCount;
+
+
+    public NumberCell(int nearByLandMineCount) {
+        this.nearByLandMineCount = nearByLandMineCount;
+    }
+
+    @Override
+    public boolean isLandMine() {
+        return false;
+    }
+
+    @Override
+    public boolean hasLandMineCount() {
+        return true;
+    }
+
+    @Override
+    public CellSnapshot getSnapshot() {
+        if(cellState.isOpened()) {
+            return CellSnapshot.ofNumber(nearByLandMineCount);
+        }
+
+        if(cellState.isFlagged()) {
+            return CellSnapshot.ofFlag();
+        }
+
+        return CellSnapshot.ofUnchecked();
+    }
+
+    @Override
+    public void flag() {
+        cellState.flag();
+    }
+
+    @Override
+    public void open() {
+        cellState.open();
+    }
+
+    @Override
+    public boolean isChecked() {
+        return cellState.isChecked();
+    }
+
+    @Override
+    public boolean isOpened() {
+        return cellState.isOpened();
+    }
+}
