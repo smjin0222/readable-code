11ddb9e8680ae8a94bc7c20ca46563fc501a52da
kn01006 <ske.kn01006@partner.skenergy.com>
2025-05-19 17:03:28 +0900

    refactor: FileHandler를 Provider 개념으로 변경

--- src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
@@ -1,9 +1,17 @@
 package cleancode.studycafe.tobe;
 
+import cleancode.studycafe.tobe.io.provider.LockerPassFileProvider;
+import cleancode.studycafe.tobe.io.provider.SeatPassFileProvider;
+import cleancode.studycafe.tobe.provider.LockerPassProvider;
+import cleancode.studycafe.tobe.provider.SeatPassProvider;
+
 public class StudyCafeApplication {
 
     public static void main(String[] args) {
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        SeatPassProvider seatPassProvider = new SeatPassFileProvider();
+        LockerPassProvider lockerPassProvider = new LockerPassFileProvider();
+
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(seatPassProvider, lockerPassProvider);
         studyCafePassMachine.run();
     }
 
